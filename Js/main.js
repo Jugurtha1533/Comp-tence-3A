414bc10d766f1d149e880cf747c644fd3d0e2b43
Jugurtha1533 <jugurthaakli@gmail.com>
2022-11-15 15:16:04 +0100

    Compétence 3A

---
+++ Js/main.js
@@ -0,0 +1,17 @@
+const { createApp } = Vue;
+createApp({
+  data() {
+    return {
+      products: []
+
+    };
+  },
+  mounted() {
+    axios
+      .get("products.json")
+      .then((response) => (this.products = response.data.products))
+  },
+}).mount("#app");
+
+
+
